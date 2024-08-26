9c2ab9589cbe5011a1e5788e09f3b28eada4b75c
cdto17 <cdanielcdto@gmail.com>
2024-08-25 22:18:19 -0500

    feat: :sparkles: Update endpoint

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     BASE_URL_MOVIES: 'http://localhost:3003/api/movies',
     BASE_URL_UPDATE: 'http://localhost:3002/api/movies',
     BASE_URL_DELETE: 'http://localhost:3001/api/movies',
-    BASE_URL_AUTH: 'http://localhost:4000/api/auth/login',
+    BASE_URL_AUTH: 'http://192.34.63.51:4000/api/auth/login',
     BASE_URL_OFFERS: 'http://localhost:3070/api/offers',
     BASE_URL_ROOMS: 'http://localhost:3020/api/rooms',
     BASE_URL_SNACKC: 'http://localhost:3011/api/snacks',
