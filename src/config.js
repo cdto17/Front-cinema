258b396052413651cb1ab45ea18a125748aadbd3
cdto17 <cdanielcdto@gmail.com>
2024-08-25 23:15:12 -0500

    feat: :white_check_mark: Update endpoints movie

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@ const config = {
     BASE_URL_AUTH: 'http://192.34.63.51:4000/api/auth/login',
     BASE_URL_OFFERS: 'http://localhost:3070/api/offers',
     BASE_URL_ROOMS: 'http://localhost:3020/api/rooms',
-    BASE_URL_SNACKC: 'http://localhost:3011/api/snacks',
-    BASE_URL_SNACKL: 'http://localhost:3012/api/snacks',
-    BASE_URL_SNACKU: 'http://localhost:3013/api/snacks',
-    BASE_URL_SNACKD: 'http://localhost:3014/api/snacks',
+    BASE_URL_SNACKC: 'http://161.35.51.225:4099/api/snacks',
+    BASE_URL_SNACKL: 'http://161.35.51.225:4004/api/snacks',
+    BASE_URL_SNACKU: 'http://161.35.51.225:4003/api/snacks',
+    BASE_URL_SNACKD: 'http://161.35.51.225:4002/api/snacks',
   };
   
   export default config;
